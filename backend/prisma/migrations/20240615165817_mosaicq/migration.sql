e300d192d6930026c1771310577c4d708962cab1
LucasGaravaglia <garavaglia.dev@gmail.com>
2024-06-15 14:08:42 -0300

    fix: Correção da migration

--- backend/prisma/migrations/20240615165817_mosaicq/migration.sql
+++ backend/prisma/migrations/20240615165817_mosaicq/migration.sql
@@ -23,7 +23,7 @@ CREATE TABLE "user" (
 );
 
 INSERT INTO "user" ("id", "name", "password", "createdAt", "updatedAt")
-VALUES ('1234-fsadfs-5342-123', 'Lucas', '123456', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('0a5be139-1f0c-4757-b478-f2f3998ad4df', 'Lucas', 'e10adc3949ba59abbe56e057f20f883e', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 -- AddForeignKey
 ALTER TABLE "list" ADD CONSTRAINT "list_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
